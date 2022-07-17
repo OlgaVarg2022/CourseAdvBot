31f03f476342cdec5e1fd114810e9e79cf77931a
Olga <olgazaydenvarg@gmail.com>
2022-07-18 00:51:22 +0300

    'first_working_solution'

---
+++ constants.py
@@ -0,0 +1,20 @@
+TOKEN = '5405403118:AAFf8Uix1_DWmsL6w-TOGtfXISSa4ECJtNM'
+URL = ' https://api.telegram.org/bot{token}/{method}'
+MY_ID = 2073215288
+
+UPDATE_METHOD = 'getUpdates'
+# UPDATE_METHOD = 'setWebhook'
+SEND_METHOD= 'sendMessage'
+
+UPDATE_ID_FILE_PATH = 'C:/Users/kalinin/olye_python/My_Bot/update_id'
+
+with open(UPDATE_ID_FILE_PATH) as file:
+    data = file.readline()
+    if data:
+        data = int(data)
+    UPDATE_ID = data
+
+WEATHER_TOKEN = 'f2897f82489f15f33dbfcb14e3b3f75b'
+WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}'
+
+
---
+++ my_weather_bot.py
@@ -0,0 +1,70 @@
+from lib2to3.pgen2 import token
+import requests
+import json
+import time
+from pprint import pprint
+import constants
+
+
+def answer_user_bot(data):
+    data = {
+        'chat_id': constants.MY_ID,
+        'text': data
+    }
+    url = constants.URL.format(token=constants.TOKEN, method=constants.SEND_METHOD)
+    response = requests.post(url, data=data)
+
+
+def parse_weather_data(data):
+    for elem in data['weather']:
+        sky = elem['main']
+    tempr = round(data['main']['temp'] - 273.15, 2)
+    wind_speed = data['wind']['speed']
+    city = data['name']
+    msg = f'{sky} in {city}, t  {tempr} C°, wind speed is {wind_speed} m/sec'
+    return msg
+
+
+def get_weather(location):
+    url = constants.WEATHER_URL.format(city=location, token=constants.WEATHER_TOKEN)
+    response = requests.get(url)
+    data = json.loads(response.content)
+    if response.status_code != 200:
+        return 'city not found'
+    return parse_weather_data(data)
+    
+
+def get_message(data):
+    return data['message']['text']
+    
+
+def save_update_id(update):
+    with open(constants.UPDATE_ID_FILE_PATH, 'w') as f:
+        f.write(str(update['update_id']))
+    constants.UPDATE_ID = update['update_id']
+    return True
+
+
+def main():
+    while True:
+        url = constants.URL.format(token=constants.TOKEN, method=constants.UPDATE_METHOD)
+        cont = requests.get(url).content
+        data = json.loads(cont)
+        result = data['result'][::-1]
+        essens_part = None
+        for elem in result:
+            if elem['message']['chat']['id'] == constants.MY_ID:
+                essens_part = elem
+                break
+                    
+        if constants.UPDATE_ID != essens_part['update_id']:            
+            message = get_message(essens_part)
+            msg = get_weather(message)
+            answer_user_bot(msg)
+            save_update_id(essens_part)        
+        # break
+        time.sleep(1)
+UPDATE_METHOD = 'getUpdates'
+
+if __name__ == '__main__':
+    main()
